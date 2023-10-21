3b031fbb30864e4d12ed9b78ce4a059ee49951a8
borisepen <borisepen@gmail.com>
2023-10-21 20:59:02 +0700

    add build_push_image_karsajobs

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+#Perintah untuk build Docker image dari berkas Dockerfile yang disediakan
+docker build -t borisepen/karsajobs:latest .
+
+#Perintah untuk login ke Docker Hub
+echo $PASSWORD_DOCKER_HUB | docker login -u borisepen --password-stdin
+
+#Perintah untuk push image ke Docker Hub
+docker push borisepen/karsajobs:latest
